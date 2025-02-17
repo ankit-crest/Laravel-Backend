682bde1fdaf3494bee119b4bfd3218c4959d6e3a
LAPTOP-O1QSPTET\AnkitTiwari <ankit.t@crestinfosystems.com>
2025-02-17 18:54:26 +0530

    Update :add category data api

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,7 +12,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+       $category = Category::all();
+
+       return response()->json($category);
     }
 
     /**
@@ -23,12 +25,20 @@ class CategoryController extends Controller
         //
     }
 
+    
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+         
+      $validate=  $request->validate([
+        'name' => 'required|max:25'
+        ]);
+
+       $category= Category::create($validate);
+
+       return response()->json($category);
     }
 
     /**
@@ -36,23 +46,34 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Category $category)
+    public function edit(Category $category,$id)
     {
-        //
-    }
+        $category = Category::find($id);
+
+        return response()->json($category);
+     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(Request $request, Category $category,$id)
     {
-        //
+       return ($id);
+       $validate=  $request->validate([
+        'name' => 'required|max:25'
+        ]);
+
+       $category= Category::where('id',$id)->update($validate);
+
+       return response()->json($category);
+
+
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -26,6 +27,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return response()->json($request->user());
 });
 
-Route::middleware('auth:sanctum')->get('/category', function (Request $request) {
-    return response()->json($request->user());
-});
+Route::middleware('auth:sanctum')->get('/category', [CategoryController::class,'index']);
+Route::middleware('auth:sanctum')->get('category/edit/{id}', [CategoryController::class,'edit']);
+Route::middleware('auth:sanctum')->put('category/update/{id}', [CategoryController::class,'update']);
+Route::middleware('auth:sanctum')->post('/category', [CategoryController::class,'store']);
